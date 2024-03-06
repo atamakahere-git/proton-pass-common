6b97e17316238fe7e8d124c70a9469bd517fc240
atamakahere <git@atamaka.maskmy.id>
2024-03-06 16:19:46 +0530

    test: remove unnecessary black_box and use then in proper place
    refer: https://bheisler.github.io/criterion.rs/book/faq.html#when-should-i-use-criterionblack_box

--- proton-pass-common/benches/card_detector.rs
+++ proton-pass-common/benches/card_detector.rs
@@ -3,12 +3,12 @@ use proton_pass_common::creditcard::CreditCardDetector;
 
 fn card_detector(c: &mut Criterion) {
     let detector = CreditCardDetector::default();
-    c.bench_function("detect unknown card", |b| b.iter(|| black_box(detector.detect("1"))));
+    c.bench_function("detect unknown card", |b| b.iter(|| detector.detect(black_box("1"))));
     c.bench_function("detect visa card", |b| {
-        b.iter(|| black_box(detector.detect("4111111111111111")))
+        b.iter(|| detector.detect(black_box("4111111111111111")))
     });
-    c.bench_function("detect Maestro card", |b| {
-        b.iter(|| black_box(detector.detect("6703 4444 4444 4449")))
+    c.bench_function("detect maestro card", |b| {
+        b.iter(|| detector.detect(black_box("6703 4444 4444 4449")))
     });
 }
 
